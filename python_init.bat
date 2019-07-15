97c48f69841dedb85b8ca982d020903d34910dbb
wangli2000_cn <wangli2000_cn@126.com>
2019-07-15 21:14:28 +0800

    20190715211400

---
+++ python_init.bat
@@ -0,0 +1,11 @@
+pip install flask
+python -m pip install --upgrade pip
+pip install sqlalchemy
+pip install psutil
+pip install psycopg2
+pip install flask_cors
+pip install neo4j
+pip install eventlet
+pip install pymssql
+pip install cx_Oracle
+pip install flask_socketio
